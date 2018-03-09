d61ed04bbce8ca70a3bdb771d66bbe8196597788
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2018-03-09 19:55:47 +0000

    more bug fixes to cart

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,28 +1,30 @@
 class OrderItemsController < ApplicationController
 
-	def create
-		@order = current_order
-		@order_item = @order.order_items.new(order_item_params)
-		@order.save
-		session[:order_id] = @order.id
-	end
+def create      
+  	@order = Order.new
+  	@order.buyer_id = current_user.id
+  	@order.seller_id = current_user.id
+  	@order.order_items.build(order_item_params)       
+  	@order.save       
+  	session[:order_id] = @order.id
+end
 
-	def update
-		@order = current_order
-		@order_item = @order.order_items.find(params[:id])
-		@order_item.update_attributes(order_item_params)
-		@order_items = @order.order_items
-	end
+def update
+	@order = current_order
+	@order_item = @order.order_items.find(params[:id])
+	@order_item.update_attributes(order_item_params)
+	@order_items = @order.order_items
+end
 
-	def destroy
-		@order = current_order
-		@order_item = @order.order_items.find(params[:id])
-		@order_item.destroy
-		@order_items = @order.order_items
-	end
+def destroy
+	@order = current_order
+	@order_item = @order.order_items.find(params[:id])
+	@order_item.destroy
+	@order_items = @order.order_items
+end
 
-	private
-		def order_item_params
-			params.require(:order_item).permit(:listing, :quantity)
-		end
+private
+    def order_item_params               
+        params.require(:order_item).permit(:listing_id, :quantity)
+    end
 end
