dd5c543ab5f9675b84757a42c954feb26fd0fe4a
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2018-04-04 21:55:13 +0100

    update styling and design

--- db/migrate/20180317095210_remove_columns_from_orders.rb
+++ db/migrate/20180317095210_remove_columns_from_orders.rb
@@ -1,7 +1,5 @@
 class RemoveColumnsFromOrders < ActiveRecord::Migration[5.1]
   def change
-    	#remove_column :orders, :listing_id, :integer
     	remove_column :orders, :seller_id, :integer
     end
-  end
 end
