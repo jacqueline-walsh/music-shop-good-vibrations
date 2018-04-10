afe062ab2376f837da475af0bc40420129159232
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2018-04-10 10:01:08 +0100

    add user to Listings

--- db/migrate/20180317095210_remove_columns_from_orders.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnsFromOrders < ActiveRecord::Migration[5.1]
-  def change
-    	remove_column :orders, :seller_id, :integer
-    end
-end
--- db/migrate/20180321155513_add_category_id_to_listings.rb
+++ db/migrate/20180321155513_add_category_id_to_listings.rb
@@ -1,5 +1,6 @@
 class AddCategoryIdToListings < ActiveRecord::Migration[5.1]
   def change
     add_column :listings, :category_id, :integer
+    add_column :listings, :user_id, :integer
   end
 end
